f1254fb032da9a1c4a40dc2df0a165611414df69
Roberta musi <musiroberta46@gmail.com>
2025-07-24 05:28:04 -0700

    practicing how to print in range in arrays using for loop

--- 2.0 software 1.0/1.0 python basis/day2/conditionals.py
+++ 2.0 software 1.0/1.0 python basis/day2/conditionals.py
@@ -9,16 +9,50 @@
 # else:
 #         print("sorry wait for the next round")
 #grade system
-grade=float(input("what is your grade:"))
-if grade >= 80:
-    print("A grade")
-elif grade >=70:
-    print(" B+ grade")
-elif grade >=60:
-    print(" grade:B")
-elif grade >=50:
-    print(" grade is: C")
-elif grade >=40:
-    print("your grade is: D")
-else:
-    print("very poor")
+# grade=float(input("what is your grade:"))
+# if grade >= 80:
+#     print("A grade")
+# elif grade >=70:
+#     print(" B+ grade")
+# elif grade >=60:
+#     print(" grade:B")
+# elif grade >=50:
+#     print(" grade is: C")
+# elif grade >=40:
+#     print("your grade is: D")
+# else:
+#     print("very poor")
+#simple calculator
+# first_number=float(input("Enter first number:"))
+# second_number =float(input("Enter second number:"))
+# operator=input("Enter operator")
+# result=0
+# if operator=="+":
+#     result=first_number + second_number
+#     print(f"Result is: {result}")
+# elif operator=="-":
+#     result=first_number - second_number
+#     print(f"Result is: {result}")
+# elif operator=="*":
+#     result=first_number * second_number
+#     print(f"Result is:{result}")
+# elif operator=="/":
+#     if second_number!=0:
+#         result=first_number / second_number
+#         print(f"Result is {result}")
+#     else:
+#         result="division by zero not possible"
+# else:
+#     print("check your operator sign")
+
+#leap year exercise
+# year=int(input("Enter year:"))
+# if year%4!=0 and year%100!=0 and year%400!=0:
+#     print("not a leap year")
+# else:
+#     print("leap year")
+# name="Roberta Musi"
+
+# parts=name.split()
+# print(f"parts is {parts}")
+
---
+++ 2.0 software 1.0/1.0 python basis/day2/loops.py
@@ -0,0 +1,36 @@
+#for loop
+#starting point
+#condition
+#increment or decrement
+
+# names =["Will", "Faith", "Musi", "aliasambom", "niambom", "fumbom"]
+# for name in names:
+#     print(name)
+#     if name.endswith("mbom"):
+#         print(f"we dan catch you: {name}")
+#     else:
+#         print(f"welcome to the party: {name}")
+
+# names =["Will", "Faith", "Musi", "aliasambom", "niambom", "fumbom"]
+# count=1
+# for name in names:
+#     print(f"{count}.{name}")
+#     count=count+1
+
+# print("counting to 5:")
+# for number in range(1, 6):
+#     print(f"count: {number}")
+
+#range function
+for i in range(5):
+    print(i)
+    #range(start, stop)
+for i in range(2 ,7):
+    print(i)
+#range(start, stop, step)
+for i in range (0, 10, 2):
+        print(i)
+#range(countdown)
+for i in range (10, 0, -1):
+     print(f"countdown: {i}")
+     print("blast off!")  
