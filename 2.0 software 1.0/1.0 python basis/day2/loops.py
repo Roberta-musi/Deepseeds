24765aa66c894dd79a759134d6d6d7dcb782f3f6
Roberta musi <musiroberta46@gmail.com>
2025-07-24 06:35:27 -0700

    more examples on loops

--- 2.0 software 1.0/1.0 python basis/day2/loops.py
+++ 2.0 software 1.0/1.0 python basis/day2/loops.py
@@ -21,16 +21,56 @@
 # for number in range(1, 6):
 #     print(f"count: {number}")
 
-#range function
-for i in range(5):
-    print(i)
-    #range(start, stop)
-for i in range(2 ,7):
-    print(i)
-#range(start, stop, step)
-for i in range (0, 10, 2):
-        print(i)
-#range(countdown)
-for i in range (10, 0, -1):
-     print(f"countdown: {i}")
-     print("blast off!")  
+# #range function
+# for i in range(5):
+#     print(i)
+#     #range(start, stop)
+# for i in range(2 ,7):
+#     print(i)
+# #range(start, stop, step)
+# for i in range (0, 10, 2):
+#         print(i)
+# #range(countdown)
+# for i in range (10, 0, -1):
+#      print(f"countdown: {i}")
+#      print("blast off!")  
+
+#basic while loop
+# count=1
+# while count<=5:
+#     print(f"count is:{count}")
+#     count+=1 #same as count=count+1
+#     #user input loop
+#     user_input=""
+#     while user_input !="quit":
+#         user_input =input("Enter 'quit' to exit:")
+#         if user_input !="quit":
+#             print(f"you entered: {user_input}")
+#     print("goodbye!!")  
+    #continue-skip to next iteration
+# print("\nprinting only odd numbers:")
+# for number in range (1, 10):
+#     if number % 2 ==0:
+#         continue #skip even numbers
+#     print(f"Odd number: {number}")
+
+#multiplication time table
+# print("Multiplication table:")
+# for i in range(1, 4): #rows
+#     for j in range(1,4): #columns
+#         result=i * j
+#         print(f"{i} x{j} ={result}")
+#         print()
+
+#pattern printing
+print("Triangle pattern")
+for row in range (1, 6):
+    for star in range(row):
+        print("*",end="")  
+    print() #new line after each      
+
+
+        
+
+    
+    
